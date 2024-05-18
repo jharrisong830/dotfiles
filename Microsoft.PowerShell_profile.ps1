d957514ca6260af457bd337c6bd062ca97137115
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-05-18 14:51:24 -0400

    adding jdk to windows path

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,7 @@ $ESC = [char]27 # usage for color: $ESC[<colorcode>m
 function prompt {
     "$ESC[31m$env:USERNAME$ESC[0m @ $ESC[32m$env:COMPUTERNAME$ESC[0m: $ESC[34m$(Split-Path -Path $pwd -Leaf)$ESC[0m > "
 }
+
+if (Test-Path -Path "C:\Users\johng\.jdks\jdk-21.0.3+9") { # adding specific jdk to path (for java, javac, etc.)
+    $env:PATH += ";C:\Users\johng\.jdks\jdk-21.0.3+9\bin"
+}
