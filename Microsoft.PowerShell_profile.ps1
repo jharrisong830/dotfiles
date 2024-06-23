bd5b49c04276538f14dc852fc6856517a78699ee
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-06-23 15:03:30 -0400

    add android studio env vars

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,3 +10,10 @@ function prompt {
 if (Test-Path -Path "C:\Users\johng\.jdks\jdk-21.0.3+9") { # adding specific jdk to path (for java, javac, etc.)
     $env:PATH += ";C:\Users\johng\.jdks\jdk-21.0.3+9\bin"
 }
+
+if (Test-Path -Path "C:\Users\johng\AppData\Local\Android\Sdk") { # android studio for react native
+    $env:ANDROID_HOME = "C:\Users\johng\AppData\Local\Android\Sdk" # create ANDROID_HOME env variable
+    if (Test-Path -Path "C:\Users\johng\AppData\Local\Android\Sdk\platform-tools") { # add platform tools to path
+        $env:PATH += "C:\Users\johng\AppData\Local\Android\Sdk\platform-tools"
+    }
+}
