803b56752643b3feb7a34cbfa9589f5f74ad38b7
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-08-31 12:39:45 -0400

    username var and postgres config

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -7,13 +7,18 @@ function prompt {
     "$ESC[31m$env:USERNAME$ESC[0m @ $ESC[32m$env:COMPUTERNAME$ESC[0m: $ESC[34m$(Split-Path -Path $pwd -Leaf)$ESC[0m > "
 }
 
-if (Test-Path -Path "C:\Users\johng\.jdks\jdk-21.0.3+9") { # adding specific jdk to path (for java, javac, etc.)
-    $env:PATH += ";C:\Users\johng\.jdks\jdk-21.0.3+9\bin"
+if (Test-Path -Path "C:\Users\$env:USERNAME\.jdks\jdk-21.0.3+9") { # adding specific jdk to path (for java, javac, etc.)
+    $env:PATH += ";C:\Users\$env:USERNAME\.jdks\jdk-21.0.3+9\bin"
 }
 
-if (Test-Path -Path "C:\Users\johng\AppData\Local\Android\Sdk") { # android studio for react native
-    $env:ANDROID_HOME = "C:\Users\johng\AppData\Local\Android\Sdk" # create ANDROID_HOME env variable
-    if (Test-Path -Path "C:\Users\johng\AppData\Local\Android\Sdk\platform-tools") { # add platform tools to path
-        $env:PATH += ";C:\Users\johng\AppData\Local\Android\Sdk\platform-tools"
+if (Test-Path -Path "C:\Users\$env:USERNAME\AppData\Local\Android\Sdk") { # android studio for react native
+    $env:ANDROID_HOME = "C:\Users\$env:USERNAME\AppData\Local\Android\Sdk" # create ANDROID_HOME env variable
+    if (Test-Path -Path "C:\Users\$env:USERNAME\AppData\Local\Android\Sdk\platform-tools") { # add platform tools to path
+        $env:PATH += ";C:\Users\$env:USERNAME\AppData\Local\Android\Sdk\platform-tools"
     }
 }
+
+if (Test-Path -Path "C:\Program Files\PostgreSQL\16") { # add psql commands to path
+    $env:PATH += ";C:\Program Files\PostgreSQL\16\bin"
+}
+
