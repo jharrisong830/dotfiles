435a0c057e516268d5f11b9516f0f305f382402c
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-08-31 14:24:33 -0400

    jdk 17 in path

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -7,8 +7,8 @@ function prompt {
     "$ESC[31m$env:USERNAME$ESC[0m @ $ESC[32m$env:COMPUTERNAME$ESC[0m: $ESC[34m$(Split-Path -Path $pwd -Leaf)$ESC[0m > "
 }
 
-if (Test-Path -Path "C:\Users\$env:USERNAME\.jdks\jdk-21.0.3+9") { # adding specific jdk to path (for java, javac, etc.)
-    $env:PATH += ";C:\Users\$env:USERNAME\.jdks\jdk-21.0.3+9\bin"
+if (Test-Path -Path "C:\Users\$env:USERNAME\.jdks\jdk-17.0.11+9") { # adding specific jdk to path (for java, javac, etc.)
+    $env:PATH += ";C:\Users\$env:USERNAME\.jdks\jdk-17.0.11+9\bin"
 }
 
 if (Test-Path -Path "C:\Users\$env:USERNAME\AppData\Local\Android\Sdk") { # android studio for react native
