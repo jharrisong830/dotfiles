d8af74e4678d584881859f28f28c88b42917bafa
John Graham <110198683+jharrisong830@users.noreply.github.com>
2025-08-16 16:51:14 -0400

    added powershell setup script

--- setup.ps1
+++ setup.ps1
@@ -7,8 +7,19 @@ $dotfiles = @{
     "nvim\" = "AppData\Local\nvim\"
     "windows\winget\settings.json" = "AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
     "windows\terminal\settings.json" = "AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+    ".bunfig.toml" = ".bunfig.toml"
 }
 
+echo "~~~ LINKING FILES... ~~~"
 foreach ($Key in $dotfiles.Keys) {
+    $DotfileRealPath = Resolve-Path $Key
+    $DotfileLinkPath = "C:\Users\$env:USERNAME\$($dotfiles[$Key])"
     
+    if (Test-Path -Path $DotfileLinkPath) {
+        echo "Unlinking/removing $DotfileLinkPath"
+        Remove-Item -Path $DotfileLinkPath -Force
+    }
+
+    echo "Adding $DotfileLinkPath, linked to $DotfileRealPath"
+    New-Item -ItemType SymbolicLink -Path $DotfileLinkPath -Target $DotfileRealPath | Out-Null
 }
