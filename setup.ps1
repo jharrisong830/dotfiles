c4e2ef3fc30e7c3b07d51ddf016350ec350a5362
John Graham <110198683+jharrisong830@users.noreply.github.com>
2025-09-17 17:21:51 -0400

    powershell setup (needs testing)

--- setup.ps1
+++ setup.ps1
@@ -10,6 +10,39 @@ $dotfiles = @{
     ".bunfig.toml" = ".bunfig.toml"
 }
 
+function _unlink_target ([string]$target) {
+    if (Test-Path -Path $target) {
+        if (! (Test-Path -Type Container -Path $target)) {
+            echo "Deleting $target"
+            Remove-Item -Path $target
+        } else {
+            echo "Deleting directory $target"
+            Remove-Item -Recurse -Force -Path $target
+        }    
+    }
+}
+
+function _link_dotfiles ([string]$filename, [string]$original_file, [string]$target) {
+    echo "[ $filename ]"
+
+    _unlink_target -target $target
+
+    echo "Linking $original_file to $target"
+    
+    # backwards naming from zsh setup, $target is the -Path where the link will be, $original_file is the -Target file to be linked to
+    New-Item -ItemType SymbolicLink -Path $target -Target $original_file 
+    echo ""
+}
+
+
+foreach ($original_file in Get-ChildItem -Path "$(Get-Location)\common\git") {
+    $filename = Split-Path -Path $original_file -Leaf
+    $target = "C:\Users\$env:USERNAME\$filename"
+
+    _link_dotfiles -filename $filename -original_file $original_file -target $target
+}
+
+
 echo "~~~ LINKING FILES... ~~~"
 foreach ($Key in $dotfiles.Keys) {
     $DotfileRealPath = Resolve-Path $Key
