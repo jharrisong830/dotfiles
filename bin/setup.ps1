0a2fb4d1bf7c419e4d2d604a2e4b08175b4c1627
John Graham <110198683+jharrisong830@users.noreply.github.com>
2025-09-18 12:44:44 -0400

    powershell setup complete

--- bin/setup.ps1
+++ bin/setup.ps1
@@ -1,15 +1,3 @@
-$dotfiles = @{
-    # dotfiles repo loc = loc relative to C:\Users\%USERNAME%
-    ".gitconfig" = ".gitconfig"
-    ".npmrc" = ".npmrc"
-    ".prettierrc" = ".prettierrc"
-    "windows\powershell\Microsoft.PowerShell_profile.ps1" = "Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
-    "nvim\" = "AppData\Local\nvim\"
-    "windows\winget\settings.json" = "AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
-    "windows\terminal\settings.json" = "AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-    ".bunfig.toml" = ".bunfig.toml"
-}
-
 function _unlink_target ([string]$target) {
     if (Test-Path -Path $target) {
         if (! (Test-Path -Type Container -Path $target)) {
@@ -26,8 +14,6 @@ function _link_dotfiles ([string]$filename, [string]$original_file, [string]$tar
     echo "[ $filename ]"
 
     _unlink_target -target $target
-
-    echo "Linking $original_file to $target"
     
     # backwards naming from zsh setup, $target is the -Path where the link will be, $original_file is the -Target file to be linked to
     New-Item -ItemType SymbolicLink -Path $target -Target $original_file 
@@ -42,17 +28,49 @@ foreach ($original_file in Get-ChildItem -Path "$(Get-Location)\common\git") {
     _link_dotfiles -filename $filename -original_file $original_file -target $target
 }
 
+foreach ($original_file in Get-ChildItem -Path "$(Get-Location)\common\js") {
+    $filename = Split-Path -Path $original_file -Leaf
+    $target = "C:\Users\$env:USERNAME\$filename"
 
-echo "~~~ LINKING FILES... ~~~"
-foreach ($Key in $dotfiles.Keys) {
-    $DotfileRealPath = Resolve-Path $Key
-    $DotfileLinkPath = "C:\Users\$env:USERNAME\$($dotfiles[$Key])"
-    
-    if (Test-Path -Path $DotfileLinkPath) {
-        echo "Unlinking/removing $DotfileLinkPath"
-        Remove-Item -Path $DotfileLinkPath -Force
-    }
-
-    echo "Adding $DotfileLinkPath, linked to $DotfileRealPath"
-    New-Item -ItemType SymbolicLink -Path $DotfileLinkPath -Target $DotfileRealPath | Out-Null
+    _link_dotfiles -filename $filename -original_file $original_file -target $target
 }
+
+
+foreach ($original_file in Get-ChildItem -Path "$(Get-Location)\windows\powershell") {
+    $filename = Split-Path -Path $original_file -Leaf
+    $target = "C:\Users\$env:USERNAME\Documents\PowerShell\$filename"
+
+    _link_dotfiles -filename $filename -original_file $original_file -target $target
+}
+
+foreach ($original_file in Get-ChildItem -Path "$(Get-Location)\windows\winget") {
+    $filename = Split-Path -Path $original_file -Leaf
+    $target = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\$filename"
+
+    _link_dotfiles -filename $filename -original_file $original_file -target $target
+}
+
+foreach ($original_file in Get-ChildItem -Path "$(Get-Location)\windows\terminal") {
+    $filename = Split-Path -Path $original_file -Leaf
+    $target = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\$filename"
+
+    _link_dotfiles -filename $filename -original_file $original_file -target $target
+}
+
+
+foreach ($original_file in Get-Item -Path "$(Get-Location)\common\nvim") {
+    $filename = Split-Path -Path $original_file -Leaf
+    $target = "$env:LOCALAPPDATA\$filename"
+
+    _link_dotfiles -filename $filename -original_file $original_file -target $target
+}
+
+foreach ($original_file in Get-Item -Path "$(Get-Location)\common\alacritty") {
+    $filename = Split-Path -Path $original_file -Leaf
+    $target = "$env:LOCALAPPDATA\$filename"
+
+    _link_dotfiles -filename $filename -original_file $original_file -target $target
+}
+
+
+echo "Done!"
